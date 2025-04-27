3a6426f1592009fd7904c8b4baaea7c503a887fb
Sudhanshu kumar <sudhanshukr1796@gmail.com>
2025-04-27 17:20:03 +0530

    adding auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,4 +22,26 @@ try{
  
 };
 
-module.exports = auth;
+const rec_auth = async function (req, res, next) {
+    try{ 
+        console.log(req.headers);
+        const token = req.headers.cookie.replace("rec_jwt=","");
+        console.log(token);
+        const verify = jwt.verify(token,process.env.USER_KEY)
+        if(!verify){
+        res.status(500).send("Error:Invalid token")
+    }
+        req._id= verify._id
+        req.token= token
+        next()
+    }catch(e){
+        res.status(500).send({
+            message:"Unsuccessfull",
+            Error:e.message
+        })
+    }
+     
+    };
+    
+
+module.exports = {auth,rec_auth};
--- routes/recruiter.js
+++ routes/recruiter.js
@@ -1,12 +1,18 @@
 const express= require("express")
+const {rec_auth}=require("../middleware/auth")
 const {sign_up,sign_in,logout}= require("../controller/recruiter_controller")
 const {rec_sign_up_validation,rec_sign_in_validation}=  require("../utils/rec_input_validation")
 
 const rec_route= express.Router()
 
-
+// sign_up
 rec_route.post("/rec/sign_up",rec_sign_up_validation,sign_up)
 
 
+// sign_in
+rec_route.post("rec/sign_in",rec_sign_in_validation,sign_in)
 
+
+// log_out
+rec_route.post("/rec/logout",rec_auth,logout)
 module.exports= rec_route
